7634acb1310c83de9a1115f95eaa2f028b6edb88
César Paulangelo <cesar.paulangelo@sansano.usm.cl>
2020-12-30 17:42:09 -0300

    added smart components with demo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { Accordion, AccordionItem } from 'smart-webcomponents-react/accordion';
+import 'smart-webcomponents-react/source/styles/smart.default.css';
 
 function App() {
   return (
@@ -9,6 +11,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+         <Accordion>  
+   <AccordionItem label="First Item">First Item Content.</AccordionItem>  
+   <AccordionItem label="Second Item">Second Item Content.</AccordionItem>  
+   <AccordionItem label="Third Item">Third Item Content.</AccordionItem>  
+   <AccordionItem label="Fourth Item">Fourth Item Content.</AccordionItem>  
+ </Accordion>  
         <a
           className="App-link"
           href="https://reactjs.org"
