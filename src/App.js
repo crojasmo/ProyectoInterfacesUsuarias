fa32602e825178590c8b4f58d8d8299a3976a3b0
César Paulangelo <cesar.paulangelo@sansano.usm.cl>
2020-12-30 18:09:15 -0300

    Added router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./header";
+import Footer from "./footer";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import { Accordion, AccordionItem } from 'smart-webcomponents-react/accordion';
 import 'smart-webcomponents-react/source/styles/smart.default.css';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-         <Accordion>  
+<BrowserRouter>
+            <Switch>
+              <Route path="/:carr/prioridad">
+                <Header/>
+                <Accordion>  
    <AccordionItem label="First Item">First Item Content.</AccordionItem>  
    <AccordionItem label="Second Item">Second Item Content.</AccordionItem>  
    <AccordionItem label="Third Item">Third Item Content.</AccordionItem>  
    <AccordionItem label="Fourth Item">Fourth Item Content.</AccordionItem>  
  </Accordion>  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+              </Route>
+              <Route path="/:carr/personal">
+                <Header/>
+              </Route>
+              <Route path="/:carr">
+                <Header/>
+              </Route>
+              <Route path="/prioridad/">
+                <Redirect to="/INF/prioridad"/>
+              </Route>
+              <Route path="/personal/">
+                <Redirect to="/INF/personal"/>
+              </Route>
+              <Route path="/">
+                <Redirect to="/INF"/>
+              </Route>
+            </Switch>
+          <Footer/>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withRouter } from "react-router"
+
+class Header extends React.Component {
+    render() {
+        return <p>Header</p>
+    }
+}
+
+export default withRouter(Header);
