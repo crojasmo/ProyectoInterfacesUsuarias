06438b36b5ae1338a4b7b95e71a4cca073e0cb1e
sgutierr <sgutierr@alumnos.inf.utfsm.cl>
2020-12-30 18:58:13 -0300

    borrando smart

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import {
   Route,
   Redirect
 } from "react-router-dom";
-import { Accordion, AccordionItem } from 'smart-webcomponents-react/accordion';
-import 'smart-webcomponents-react/source/styles/smart.default.css';
+
 
 
 
@@ -19,12 +18,7 @@ function App() {
             <Switch>
               <Route path="/:carr/prioridad">
                 <Header/>
-                <Accordion>  
-   <AccordionItem label="First Item">First Item Content.</AccordionItem>  
-   <AccordionItem label="Second Item">Second Item Content.</AccordionItem>  
-   <AccordionItem label="Third Item">Third Item Content.</AccordionItem>  
-   <AccordionItem label="Fourth Item">Fourth Item Content.</AccordionItem>  
- </Accordion>  
+                 
               </Route>
               <Route path="/:carr/personal">
                 <Header/>
