7722da769611eaaec1d58a63cb39b03105c60d50
sgutierr <sgutierr@alumnos.inf.utfsm.cl>
2021-01-03 17:58:02 -0300

    prototipo 1.1

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -59,7 +59,22 @@ class Contacto extends React.Component {
         
             </Col>
 
-            <Col>aaaaaaaa</Col>
+            <Col>
+            <div class="container">
+            <h2>Card Image</h2>
+            <p>Image at the top (card-img-top):</p>
+            <div class="col d-flex justify-content-center">
+            <div class="card" style={{width: '18rem'}}>
+                <img class="card-img-top" src="..." alt="Card image cap"/>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            </div>
+            </div>
+            </Col>
             </Row>
 
         
