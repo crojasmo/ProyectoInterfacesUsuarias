5d970b797b0b51ca2f903f6a9ff574f16bf6b390
Carlos <carlos.rojasmo@sansano.usm.com>
2021-01-03 18:14:18 -0300

    trueque - Agregar

--- src/Components/Trueque.js
+++ src/Components/Trueque.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Container from "react-bootstrap/cjs/Container";
 import PropTypes from "prop-types"
 import { withRouter } from "react-router"
-import {Col, FormControl, InputGroup, Row,ButtonGroup,Button,Card,Table} from "react-bootstrap";
+import {Col, FormControl, InputGroup, Row,ButtonGroup,Button,Card,Table,Figure} from "react-bootstrap";
+import imagen from "./img/auto.jpg"
 
 class Trueque extends React.Component {
 
@@ -14,34 +15,78 @@ class Trueque extends React.Component {
 
     constructor(props) {
         super(props);
-        this.careers = {}
-        this.numbers = [1, 2, 3, 4, 5]
+        this.state = {
+            tusproductos:[{"nombre":"autito","precio":2200},{"nombre":"estufa","precio":21300}],
+            productosTrueque:[],
+            ValorTrueque:-0
+        }
+        /*this.CreateProducto("autito",2200)
+        this.CreateProducto("estufa",21300)
+        this.CreateProducto("telefono",99000)
+        this.CreateProducto("notebook",500000)
+        this.CreateProducto("auto",1290000)*/
+        
     }
+    
+    CreateProducto(nombre,precio){
+        var producto=[...this.state.tusproductos]
+        var prod={"nombre":nombre,"precio":precio}
+        producto.push(prod)
+        this.setState({tusproductos:producto})
+    }
+
+    Ofrecer(producto){
+        var listaProducto=[...this.state.tusproductos]
+        var ofrecidos=[...this.state.productosTrueque]
+        var valor=this.state.ValorTrueque
+        const index = listaProducto.indexOf(producto);
+        if (index > -1) {
+            listaProducto.splice(index, 1);
+        }
+        ofrecidos.push(producto)
+        valor=valor+producto.precio
+        this.setState({tusproductos:listaProducto,productosTrueque:ofrecidos,ValorTrueque:valor})
+
+
+    }
+
+    
+
+    
     
 
     render() {
         
         return <>
-         
-            <Container fluid={true}>
-            <Col>
-                <Row><h1>
+            
+            <Container fluid>
+            <Row >
+            <Col xs={2} classname="align-self-start">
+                <h1>
                     Sus Productos
                 </h1>
                 
-                </Row>
+                
                 <br/>
                 <br/>
-                <Row>
+                
+                <Card body>
                 <ButtonGroup vertical>
-                {this.numbers.map(numero =>
-                     <Button> {numero}</Button>
-
+                {this.state.tusproductos.map(producto =>
+                     <Button variant="outline-secondary" type="button" onClick={this.Ofrecer.bind(this,producto)}> <Figure>
+                     <Figure.Image
+                       width={50}
+                       height={50}
+                
+                       src={imagen}
+                     />
+                   </Figure> {producto.nombre} ---- {producto.precio}</Button>
+                    
 
                 )}
-
                 </ButtonGroup>
-                </Row>
+                </Card>
+                
                 <br/>
                
                 <br/>
@@ -49,8 +94,38 @@ class Trueque extends React.Component {
         
             </Col>
 
+        <Col xs={3} classname="align-self-end">
+        <h1>
+                    Productos para trueque
+                </h1>
+                
+               
+                <br/>
+                <br/>
+                <Card body>
+                <ButtonGroup vertical>
+                {this.state.productosTrueque.map(producto =>
+                     <Button variant="outline-danger" size="lg"> <Figure>
+                     <Figure.Image
+                       width={50}
+                       height={50}
+                
+                       src={imagen}
+                     />
+                   </Figure> {producto.nombre} ---- {producto.precio}</Button>
+                    
+
+                )}
+                </ButtonGroup>
+                </Card>
+                <br/>
+               
+                <br/>
+                <p>{this.state.ValorTrueque}</p>
         
+        </Col>
         
+        </Row>
         </Container>  
         </>
 
