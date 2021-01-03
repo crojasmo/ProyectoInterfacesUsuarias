2ade5ee219d5ce6999f86b812f68f1dabb19cebc
sgutierr <sgutierr@alumnos.inf.utfsm.cl>
2021-01-03 18:58:21 -0300

    prototipo contacto 1.2

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -29,6 +29,7 @@ class Contacto extends React.Component {
                 
                 <br/>
                 <br/>
+                <div class="scrollbar" style = {{ width: "800px", maxHeight: "400px" }}>
                 <div class="container">
                 <img src="/w3images/bandmember.jpg" alt="Avatar"/>
                 <p>Hello. How are you today?</p>
@@ -52,28 +53,50 @@ class Contacto extends React.Component {
                   <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
                   <span class="time-left">11:05</span>
                 </div> 
+                <div class="container">
+                  <img src="/w3images/bandmember.jpg" alt="Avatar"/>
+                    <p>Sweet! So, what do you wanna do today?</p>
+                  <span class="time-right">11:02</span>
+                </div>
+
+                <div class="container darker">
+                 <img src="/w3images/avatar_g2.jpg" alt="Avatar" class="right"/>
+                  <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
+                  <span class="time-left">11:05</span>
+                </div> 
+                </div>
                 <br/>
-               
+                <InputGroup className="mb-3">
+                <InputGroup.Prepend>
+                <Button variant="success">Button</Button>
+                </InputGroup.Prepend>
+                <FormControl  placeholder="Maximo 100 caracteres" maxLength ={100} aria-label="Recipient's username" aria-describedby="basic-addon1" />
+                <InputGroup.Append>
+                <Button variant="outline-primary">Send</Button>
+                </InputGroup.Append>
+                
+                </InputGroup>
                 <br/>
                 
         
             </Col>
 
             <Col>
-            <div class="container">
-            <h2>Card Image</h2>
-            <p>Image at the top (card-img-top):</p>
+            
+            <h2>Vendedor</h2>
+            
             <div class="col d-flex justify-content-center">
             <div class="card" style={{width: '18rem'}}>
                 <img class="card-img-top" src="..." alt="Card image cap"/>
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">Juan Rojas</h5>
+                    <p class="card-text">Telefono: +569664020xx</p>
+                    <p class="card-text">Ubicacion:Region Metropolitana,Santiago</p>
+                    <a href="#" class="btn btn-primary">Perfil</a>
                 </div>
             </div>
             </div>
-            </div>
+        
             </Col>
             </Row>
 
