8edea9430e373f4408dd77ab08ef162863607e82
sgutierr <sgutierr@alumnos.inf.utfsm.cl>
2021-01-07 23:20:34 -0300

    contacto no fluido

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -62,7 +62,7 @@ class Contacto extends React.Component {
         return <>
          
          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-            <Container fluid={true}>
+            <Container >
             <Row>
             <Col>
                 
