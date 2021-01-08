6e8ff5403c01b987787d9d2856218cedc773178d
sgutierr <sgutierr@alumnos.inf.utfsm.cl>
2021-01-08 20:05:34 -0300

    mensaje por defecto arreglado

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -74,39 +74,15 @@ class Contacto extends React.Component {
                 <br/>
                 <br/>
                 <div class="scrollbar" ref={this.chat}  style = {{maxHeight: "400px" }}>
-                <div class="kontainer">
-                <img src="/w3images/bandmember.jpg" alt="Avatar"/>
-                <p>Hello. How are you today?</p>
-                <span class="time-right">11:00</span>
-                </div>
-
                 <div class="kontainer darker">
-                <img src="/w3images/avatar_g2.jpg" alt="Avatar" class="right"/>
-                <p>Hey! I'm fine. Thanks for asking!</p>
-                <span class="time-left">11:01</span>
+                <img src="/w3images/bandmember.jpg" alt="Avatar" class="right"/>
+                <p>Hola, mucho gusto</p>
+                <span class="time-left">11:00</span>
                 </div>
-
-                <div class="kontainer">
-                  <img src="/w3images/bandmember.jpg" alt="Avatar"/>
-                    <p>Sweet! So, what do you wanna do today?</p>
-                  <span class="time-right">11:02</span>
-                </div>
-
                 <div class="kontainer darker">
-                 <img src="/w3images/avatar_g2.jpg" alt="Avatar" class="right"/>
-                  <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
-                  <span class="time-left">11:05</span>
-                </div> 
-                <div class="kontainer">
-                  <img src="/w3images/bandmember.jpg" alt="Avatar"/>
-                    <p>Sweet! So, what do you wanna do today?</p>
-                  <span class="time-right">11:02</span>
-                </div>
-
-                <div class="kontainer darker">
-                 <img src="/w3images/avatar_g2.jpg" alt="Avatar" class="right"/>
-                  <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
-                  <span class="time-left">11:05</span>
+                <img src="/w3images/bandmember.jpg" alt="Avatar" class="right"/>
+                <p>Pregunte y yo respondere sus dudas tan pronto como me sea posible</p>
+                <span class="time-left">11:00</span>
                 </div> 
                 </div>
                 <br/>
